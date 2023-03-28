6a086362995bec057ef0d9a90936e0d4028008e5
Krishna Pradhan <kpaccess@gmail.com>
2023-03-28 13:40:42 -0400

    moved edit post states and edit functionality from context to Edit component

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,14 +1,21 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import { format } from 'date-fns';
+import { useNavigate } from 'react-router-dom';
 
 import DataContext from '../context/DataContext';
+import api from '../api/posts';
 
 const EditPost = () => {
-  const { posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle } =
-    useContext(DataContext);
+  const { posts, setPosts } = useContext(DataContext);
   const { id } = useParams();
 
   const post = posts.find((post) => post.id.toString() === id);
+
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (post) {
@@ -16,6 +23,29 @@ const EditPost = () => {
       setEditBody(post.body);
     }
   }, [post, setEditTitle, setEditBody]);
+
+  const handleEdit = async (id) => {
+    try {
+      const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+      const updatedPost = {
+        id,
+        title: editTitle,
+        body: editBody,
+        datetime,
+      };
+
+      const response = await api.put(`/posts/${id}`, updatedPost);
+
+      setPosts(
+        posts.map((post) => (post.id === id ? { ...response.data } : post))
+      );
+      setEditBody('');
+      setEditTitle('');
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
 
   return (
     <main className="EditPost">
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,7 +1,4 @@
 import { createContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { format } from 'date-fns';
-import api from '../api/posts';
 
 import useAxiosFetch from '../hooks/useAxiosFetch';
 
@@ -11,11 +8,6 @@ export const DataProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
-
-  const [editTitle, setEditTitle] = useState('');
-  const [editBody, setEditBody] = useState();
-
-  const navigate = useNavigate();
 
   const { data, fetchError, isLoading } = useAxiosFetch(
     'http://localhost:3500/posts'
@@ -34,31 +26,6 @@ export const DataProvider = ({ children }) => {
     setSearchResults(filteredResults.reverse());
   }, [posts, search]);
 
-  useEffect(() => {}, []);
-
-  const handleEdit = async (id) => {
-    try {
-      const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-      const updatedPost = {
-        id,
-        title: editTitle,
-        body: editBody,
-        datetime,
-      };
-
-      const response = await api.put(`/posts/${id}`, updatedPost);
-
-      setPosts(
-        posts.map((post) => (post.id === id ? { ...response.data } : post))
-      );
-      setEditBody('');
-      setEditTitle('');
-      navigate('/');
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-    }
-  };
-
   return (
     <DataContext.Provider
       value={{
@@ -69,11 +36,6 @@ export const DataProvider = ({ children }) => {
         isLoading,
         posts,
         setPosts,
-        editTitle,
-        editBody,
-        setEditBody,
-        setEditTitle,
-        handleEdit,
       }}
     >
       {children}
