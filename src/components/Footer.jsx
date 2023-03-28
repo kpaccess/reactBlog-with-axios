3eb4709f384e6e8ee03782a67a8ba4426cb73a65
Krishna Pradhan <kpaccess@gmail.com>
2023-03-27 20:49:09 -0400

    style change and footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const Footer = ({ length }) => {
+  const date = new Date();
   return (
     <footer className="Footer">
-      <p>
-        {length} {length > 1 ? 'items' : 'item'}{' '}
-      </p>
+      <p>Copyright &copy; {date.getFullYear()}</p>
     </footer>
   );
 };
